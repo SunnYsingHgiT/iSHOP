35df436d58681d68d49389eaee0d246242b14a47
Sunny <im.singhsunny053@gmail.com>
2022-08-25 21:49:18 +0530

    created file

---
+++ php_login/chatbot/static/scripts/responses.js
@@ -0,0 +1,15 @@
+function getBotResponse(input) {
+  if (input == "Hi") {
+    return "How can I help you?";
+  } else if (input == "Need help") {
+    return "Do you have query?";
+  } else if (input == "Yes") {
+    return "Want to talk to an agent";
+  } else if (input == "Yes") {
+    return "Hey I am happy to inform you that one of our executive will contact you in 5 minutes on your registered phone number";
+  } else if (input == "yes") {
+    return "Hey I am happy to inform you that one of our executive will contact you in 5 minutes on your registered phone number";
+  } else {
+    return "Try asking something else!";
+  }
+}
